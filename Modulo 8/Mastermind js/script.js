2a1c16ff4434c31ab732d03e8c9036ec2171ef36
JTarifa <conexiontarifa@gmail.com>
2023-06-07 21:44:46 +0200

    integración modulo 8

--- Modulo 8/Mastermind js/script.js
+++ Modulo 8/Mastermind js/script.js
@@ -1,1 +1,156 @@
-// TODO: ALL
+function createBoard(){
+    return {
+        validColors: ["BLANCO", "NEGRO", "AMARILLO", "VERDE", "ROJO", "AZUL", "NARANJA"],
+        winCombination: [],
+        createWinCombination: function(){
+            let winCombination = [];
+            do{
+                let repeat = false;
+                const index = Math.floor(Math.random() * this.validColors.length);
+                for(let i = 0; i < winCombination.length; i++){
+                    if(winCombination[i] === this.validColors[index]){
+                        repeat = true;
+                    }
+                }
+                if(!repeat){
+                    winCombination.push(this.validColors[index]);
+                }
+            }while(winCombination.length < 5)
+    
+            console.log(winCombination[0] + " " + winCombination[1] + " " + winCombination[2] + " " + winCombination[3] + " " + winCombination[4]);
+            this.winCombination = winCombination;
+        },
+        checkCombinacionHits: function(InputCombination){
+            let supportCombination = ["VACIO", "VACIO", "VACIO", "VACIO", "VACIO"];
+            for(let i = 0; i < InputCombination.length; i++){
+                if(InputCombination[i] === this.winCombination[i]){
+                    supportCombination[i] = "BLANCO";
+                }else{
+                    let encontrado = false;
+                    for(let j = 0; j < this.winCombination.length && !encontrado; j++){
+                        if(InputCombination[i] === this.winCombination[j]){
+                            supportCombination[i] = "NEGRO";
+                            encontrado = true;
+                        }
+                    }
+                }
+            }
+            return supportCombination;
+        },
+        win: function(hits){
+            let nColoresAcertados = 0;
+            for(let i = 0; i < hits.length; i++){
+                if(hits[i] === "BLANCO"){
+                    nColoresAcertados += 1;
+                }
+            }
+            if (nColoresAcertados == 5) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
+function createGame(){
+    return {
+        mov_restantes: 15,
+        validColors: ["BLANCO", "NEGRO", "AMARILLO", "VERDE", "ROJO", "AZUL", "NARANJA"],
+        combinacionesPropuestas: [],
+        hits: [],
+        win: false,
+        inputCombination: NaN,
+        board: createBoard(),
+        combinacionGanadora: [],
+        init: function(){
+            // game.combinacionGanadora = getWinCombination();
+            this.board.createWinCombination();
+            //const combinacionIngresada;
+            do {
+                const combinacionIngresada = getCombinacion(this);
+                this.hits = this.board.checkCombinacionHits(combinacionIngresada, this.board.winCombination);
+                this.win = this.board.win(this.hits);
+                updateHistorial(combinacionIngresada, this.hits, this);
+        
+                if (!this.win) {
+                    this.mov_restantes -= 1;
+                }
+            } while (!gameEnd(this)); // nor, si las dos son falsas repite bucle
+            printMessageEndGame(this);        
+        }
+
+    }
+
+    function getCombinacion(object){
+        let vaildCombination;
+                
+        do{                     
+            vaildCombination = false;
+            let entrada = prompt("Agrega una combinacion de 5 colores validos: ");
+            object.inputCombination = entrada.split(' ');
+
+            let nColoresValid = 0;
+            for(let i = 0; i < object.inputCombination.length; i++){
+                let colorValido = false;
+                for (let j = 0; j < object.validColors.length && !colorValido ; j++){
+                    if(object.inputCombination[i] === object.validColors[j]){
+                        colorValido = true;
+                    }
+                }
+                if(colorValido){
+                    nColoresValid += 1;
+                }
+            }
+
+            const nColors = 5;
+            if(nColoresValid == nColors){
+                vaildCombination = true;
+            } 
+        }while(!vaildCombination)
+        return object.inputCombination;
+    }
+
+    function updateHistorial(InputCombination, aciertos, object){
+        object.combinacionesPropuestas.push(InputCombination);
+        object.hits.push(aciertos)
+        console.log(InputCombination + " -- " + aciertos );
+    }
+
+    function gameEnd(object){
+        return object.win || object.mov_restantes <= 0;  
+    }
+
+    function printMessageEndGame(object){
+        object.win ? console.log("ENORABUENA EL JUGAODR HA GANADO"):
+                 console.log("NO HAY MAS MOVIMIENTOS POSIBLE, HAS PERDIDO");        
+    }
+}
+
+function createApp(){
+    return {
+        game: createGame(),
+        start: function(){
+            do{
+                this.game.init();
+            }while(playAgain())    
+            console.log("PROGRAMA END"); 
+        }
+    }
+
+    function playAgain(){
+        let aswerd;
+        let isAnswerdValid;
+        do{
+            aswerd = prompt('Quieres jugar otra partida (SI/NO): ');
+            isAnswerdValid = aswerd === 'SI' || aswerd === 'NO';
+            if (!isAnswerdValid){
+                console.log("LA RESPUESTA NO ES VALIDA, SOLO SE ACEPTA SI O NO EN MAYUSCULAS");
+            }
+        }while(!isAnswerdValid)
+     
+        return aswerd === 'SI';
+    }        
+}
+
+let app = createApp();
+app.start();
--- Modulo 8/Tic Tac Toe js/script.js
+++ Modulo 8/Tic Tac Toe js/script.js
@@ -1,1 +1,174 @@
-// TODO: ALL
+// ------------------- START PROGRAMA -------------------
+function createApp(){
+    return {
+        game: createGame(),
+        start: function(){
+            do{
+                this.game.init();
+            }while(playAgain())    
+            console.log("PROGRAMM END");  
+        }
+    }
+
+    function playAgain(){
+        let answer;
+        let isAnswerdValid;
+        do{
+            answer = prompt('Quieres jugar otra partida(SI/NO): ');
+            isAnswerdValid = answer === 'SI' || answer === 'NO';
+            if (!isAnswerdValid){
+                console.log("LA RESPUESTA NO ES VALIDA, SOLO SE ACEPTA SI O NO EN MAYUSCULAS");
+            }
+        }while(!isAnswerdValid)    
+        return answer === 'SI'; 
+    }
+}
+
+function createGame(){
+    return {
+        TOKEN_STATE: ['X', 'Y'],
+        N_PLAYERS: 2,
+        player: 0,
+        mov_restantes: 9,
+        board: NaN,// = new Tablero(3, 3);
+        win: false, 
+        init: function(){
+            this.board = createBoard(3, 3);
+            this.board.inicializeTablero(3, 3);
+            this.board.print();
+            
+            do {
+                nextThrow(this);
+                if (!this.win) {
+                    this.player = (this.player + 1) % this.N_PLAYERS;
+                    this.mov_restantes -= 1;
+                }
+            }while(!gameEnd(this))
+            printMessageEndGame(this);
+        },
+    }
+
+    function gameEnd(object){
+        return object.win || object.mov_restantes <= 0;
+    }
+
+    function nextThrow(object) {
+        let { file, column } = getFileColumn(object);
+    
+        object.board.update(file, column, object.TOKEN_STATE[object.player]);
+        object.board.print();
+    
+        object.win = playerWin(object);
+        console.log("TIRADA END")
+    }
+
+    function playerWin(object) {      
+        const winConditions = [
+          [[0, 0], [0, 1], [0, 2]], // Rows
+          [[1, 0], [1, 1], [1, 2]],
+          [[2, 0], [2, 1], [2, 2]],
+          [[0, 0], [1, 0], [2, 0]], // Columns
+          [[0, 1], [1, 1], [2, 1]],
+          [[0, 2], [1, 2], [2, 2]],
+          [[0, 0], [1, 1], [2, 2]], // Diagonals
+          [[2, 0], [1, 1], [0, 2]]
+        ];
+      
+        for(const cells of winConditions) {
+            let hits = 0;
+            for (const [row, col] of cells) {
+                if (object.board.getCell(row, col,) === object.TOKEN_STATE[object.player]){
+                    hits += 1;
+                }
+            }
+            if (hits === 3){
+                return true;
+            }
+        }
+        return false;
+      }
+
+    function getFileColumn(object) {
+        let file, column;
+        do {
+            file = getPosition("FILE");
+            column = getPosition("COLUMN");
+        } while(!object.board.isPositionValid(file, column));
+        console.log("SE HA ELEGIDO LA FILA: " + file);
+        console.log("SE HA ELEGIDO LA COLUMNA: " + column);
+        return { file, column };
+    }
+
+    function getPosition(fileOColumn){
+        let fileColumn = parseInt(prompt("Agrega una posicion valida del tablero " + fileOColumn + " 1-2-3"));
+        const OFFSET = 1;
+        console.log(fileColumn);
+        return fileColumn - OFFSET;
+    }
+
+    function printMessageEndGame(object){
+        const PLAYERS = ["PLAYER 1", "PLAYER 2"];
+                (object.win) ? console.log("ENORABUENA EL JUGADOR: " + PLAYERS[object.player] + " HA GANADO"): console.log("NO HAY MAS MOVIMIENTOS POSIBLE, EMPATE");       
+    }
+}
+
+function createBoard(files, columns){
+    return {
+        files: files,
+        columns: columns,
+        boardState: NaN,
+        inicializeTablero: function(files, columns){
+            let newBoard = new Array(files);
+            for(let i = 0; i < files; i++){
+                newBoard[i] = new Array(columns);
+            }
+        
+            for(let i = 0; i < files; i++){             
+                for (let j = 0; j < columns; j++){
+                    newBoard[i][j] = "0";
+                }
+            } 
+            this.boardState = newBoard;
+        }, 
+        update: function(file, column, token){
+            this.boardState[file][column] = token;
+        }, 
+        getCell: function(file, column){
+            return this.boardState[file][column];
+        }, 
+        isPositionValid: function(file, column){
+            if (validatePositionRange(file, column, this)){
+                return isEmpy(file, column, this);            
+            } 
+            return false;
+        },
+        print: function(){
+            console.log("check inside funciton print");
+            console.log(this.boardState[0]);
+            console.log(this.boardState[1]);
+            console.log(this.boardState[2]);
+        }    
+    }
+
+    function isEmpy(file, column, object){
+        if(object.boardState[file][column] === "0"){
+            return true;
+        }
+        console.log("LA POSICION ELEGIDA NO ES VALIDA POR QUE YA ESTA OCUPADA");
+        return false;
+    }
+
+    function validatePositionRange(file, column){
+        let validFile = 0 <= file && file < (file + 1);
+        let validColumn = 0 <= column && column < (column + 1);
+
+        if(validFile && validColumn){
+            return true;
+        } 
+        console.log("LA POSICION NO ES VALIDA, NO ESTA DENTRO DEL TABLERO");
+        return false;
+    }
+}
+
+let app = createApp();
+app.start();
